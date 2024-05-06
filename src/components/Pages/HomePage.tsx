c2bbfd5efbcd6b81c08184ddba4a388a0e384019
SofTest94 <165555006+SofTest94@users.noreply.github.com>
2024-05-06 00:07:45 -0600

    Delete src directory

--- src/components/Pages/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-// src/components/pages/HomePage.tsx
-import React from 'react';
-import MainTemplate from '../templates/MainTemplate';
-
-const HomePage: React.FC = () => {
-  return <MainTemplate />;
-};
-
-export default HomePage;
--- src/components/types/specialities.ts
+++
@@ -1,4 +0,0 @@
-export interface Specialties {
-  _id: string;
-  name: string;
-}
