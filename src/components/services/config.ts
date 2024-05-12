c0059195bdfc484a5a580c5b7ae27494105a8746
harmonydesarrollo <168995150+harmonydesarrollo@users.noreply.github.com>
2024-05-11 21:02:58 -0600

    cambio de url heroku de softest a la de baushell

--- src/components/services/config.ts
+++ src/components/services/config.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
-
-// const URL_BASE = process.env.API_URL_BASE;
-const URL_BASE = 'https://dev-harmony-05492453a3dd.herokuapp.com/';
+// produccion baushell
+const URL_BASE = 'https://harmony-production-ff6ebeae2992.herokuapp.com/';
 
 const HarmonyApi = axios.create({
   baseURL: URL_BASE,
