68224ddfb36395d2249b21ead628f7a89a21053c
harmonydesarrollo <168995150+harmonydesarrollo@users.noreply.github.com>
2024-05-11 22:10:18 -0600

    Update TextContent.tsx

--- src/components/Pages/Body/SubPages/TextContent.tsx
+++ src/components/Pages/Body/SubPages/TextContent.tsx
@@ -22,7 +22,7 @@ const TextContent: React.FC = () => {
           </p>
           <div style={{ marginTop: '15%' }}>
             <Button
-              onClick={() => handleScheduleAppointmentClick('7711129510', 'Buenas tardes, me gustaria agendar una cita.')}
+              onClick={() => handleScheduleAppointmentClick('5576877703', '¡Hola! Bienvenido a Harmony Therapy. Estamos aquí para ayudarte a encontrar la felicidad y libertad de movimiento. Por favor, déjanos tu mensaje y te responderemos pronto. ¡Gracias por elegirnos!')}
               className={'section-btn'}
             >
               Agendar cita
