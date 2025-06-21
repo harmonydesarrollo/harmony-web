e99d755b4a90ac1a570340603abbf3aa9c7460eb
SofTest <softest94@gmail.com>
2025-06-21 10:05:30 -0600

    se actualizo 2025

--- src/components/services/config.ts
+++ src/components/services/config.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //produccion
 // const URL_BASE = 'https://harmony-production-ff6ebeae2992.herokuapp.com/'; // anterior ya no fucniona
-const URL_BASE = 'https://harmony-api-prod-4a08fe78d33b.herokuapp.com1/'; // 20-06-2025
+const URL_BASE = 'https://harmony-api-prod-4a08fe78d33b.herokuapp.com/'; // 20-06-2025
 
 const HarmonyApi = axios.create({
   baseURL: URL_BASE,
